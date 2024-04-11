1975d67eef37c812bbd098928ac2485e7aea5cdf
Camilo Zapata <110652931+Zp07@users.noreply.github.com>
2024-04-11 06:02:20 -0500

    Delete dgogoWeb directory

--- dgogoWeb/src/Imagenes/Imagenes.js
+++
@@ -1,9 +0,0 @@
-import logo from './assets/imgs/logo.png';
-import exotica from './assets/imgs/exotica.png'
-import carrito from './assets/imgs/carrito.png'
-
-export default {
-    'logo': logo,
-    'exotica' : exotica,
-    'carrito' : carrito
-}
